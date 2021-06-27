f6f33fc77c39a0e63fc2b302ffebfea383adf4ba
krystalwhite <krystalsnowhite@gmail.com>
2021-06-27 15:27:29 -0500

    part 8 but search is still not working

--- Unit1/orbit-report/src/app/app.component.ts
+++ Unit1/orbit-report/src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   title = 'orbit-report';
 
   sourceList: Satellite[];
-  // displayList: Satellite[];
+  displayList: Satellite[];
 
 constructor() {
   // this.sourceList = [
@@ -24,7 +24,7 @@ constructor() {
 
   
   this.sourceList = [];
-  // this.displayList = [];
+  this.displayList = [];
    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
    window.fetch(satellitesUrl).then(function(response) {
@@ -38,25 +38,24 @@ constructor() {
           this.sourceList.push(satellite);
          }
 
-         
+         this.displayList = this.sourceList.slice(0);
       }.bind(this));
    }.bind(this));
-
 }
 
-// search(searchTerm: string): void {
-//   let matchingSatellites: Satellite[] = [];
-//   searchTerm = searchTerm.toLowerCase();
-//   for(let i=0; i < this.sourceList.length; i++) {
-//      let name = this.sourceList[i].name.toLowerCase();
-//      if (name.indexOf(searchTerm) >= 0) {
-//         matchingSatellites.push(this.sourceList[i]);
-//      }
-//   }
-//   // assign this.displayList to be the array of matching satellites
-//   // this will cause Angular to re-make the table, but now only containing matches
-//   this.displayList = matchingSatellites;
-// }
+search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
 
 
 }
